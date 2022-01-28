02678d660e9fb7c0f27c3c029f2e322a9769a40b
Noamaan Mulla <noamaan.mulla03@ggmail.com>
2022-01-28 23:41:40 +0530

    Created header tabs for switching between delivery and pickup

---
+++ components/HeaderTabs.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { View, Text, StatusBar, TouchableOpacity, StyleSheet } from 'react-native';
+
+export default function HeaderTabs() {
+    const [activeTab, setActiveTab] = useState("Delivery")
+
+    return (
+        <View style={styles.headerContainer}>
+            <HeaderButton buttonName="Delivery" activeTab={activeTab} setActiveTab={setActiveTab} />
+            <HeaderButton buttonName="Pickup" activeTab={activeTab} setActiveTab={setActiveTab} />
+        </View>
+    );
+}
+
+const HeaderButton = ({ buttonName, activeTab, setActiveTab }) => (
+    <TouchableOpacity style={{ backgroundColor: activeTab === buttonName ? "#000000" : "#ffffff", ...styles.headerButton, }} onPress={() => setActiveTab(buttonName)}>
+        <Text style={{ color: activeTab === buttonName ? "#ffffff" : "#000000", ...styles.headerButtonText, }}>{buttonName}</Text>
+    </TouchableOpacity>
+)
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        paddingTop: StatusBar.currentHeight,
+        flexDirection: "row",
+        alignSelf: "center"
+    },
+    headerButton: {
+        paddingVertical: 6,
+        paddingHorizontal: 16,
+        borderRadius: 30,
+    },
+    headerButtonText: {
+        fontSize: 15,
+        fontWeight: "700"
+    }
+})
