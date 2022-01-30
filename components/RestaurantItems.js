a5059c9cc9ebb676bdca2daf06125738344a85ca
Noamaan Mulla <noamaan.mulla03@gmail.com>
2022-01-30 18:43:16 +0530

    Added hardcoded data for testing RestaurantItems component

--- components/RestaurantItems.js
+++ components/RestaurantItems.js
@@ -2,34 +2,66 @@ import { View, Image, StyleSheet, TouchableOpacity, Text } from 'react-native';
 import React from 'react';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default function RestaurantItem() {
+const localRestaurants = [
+    {
+        name: "Beachside Bar",
+        image_url:
+            "https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg",
+        categories: ["Cafe", "Bar"],
+        price: "$$",
+        reviews: 1244,
+        rating: 4.5,
+    },
+    {
+        name: "Benihana",
+        image_url:
+            "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        categories: ["Cafe", "Bar"],
+        price: "$$",
+        reviews: 1244,
+        rating: 3.7,
+    },
+    {
+        name: "India's Grill",
+        image_url:
+            "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        categories: ["Indian", "Bar"],
+        price: "$$",
+        reviews: 700,
+        rating: 4.9,
+    },
+];
+
+export default function RestaurantItems() {
     return (
         <TouchableOpacity style={styles.restaurantItemContainer} activeOpacity={1}>
-            <View style={styles.restaurantItem}>
-                <RestaurantImage />
-                <RestaurantInfo />
-            </View>
+            {localRestaurants.map((restaurant, index) => (
+                <View style={styles.restaurantItem} key={index}>
+                    <RestaurantImage image_url={restaurant.image_url} />
+                    <RestaurantInfo name={restaurant.name} rating={restaurant.rating} />
+                </View>
+            ))}
         </TouchableOpacity>
     );
 }
 
-const RestaurantImage = () => (
+const RestaurantImage = ({ image_url }) => (
     <React.Fragment>
-        <Image style={styles.restaurantImage} source={{ uri: 'https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg' }} />
+        <Image style={styles.restaurantImage} source={{ uri: image_url }} />
         <TouchableOpacity style={styles.restaurantImageHeartIcon}>
             <MaterialCommunityIcons name="heart-outline" size={25} color="#ffffff" />
         </TouchableOpacity>
     </React.Fragment>
 )
 
-const RestaurantInfo = () => (
+const RestaurantInfo = ({ name, rating }) => (
     <View style={styles.restaurantInfo}>
         <View>
-            <Text style={styles.restaurantInfoTitle}>Farmhouse Kitchen Thai Cuisine</Text>
+            <Text style={styles.restaurantInfoTitle}>{name}</Text>
             <Text style={styles.restaurantInfoCookingTime}>30 - 45 · min</Text>
         </View>
         <View style={styles.restaurantInfoItemRating}>
-            <Text>4.5</Text>
+            <Text>{rating}</Text>
         </View>
     </View>
 )
--- screens/Home.js
+++ screens/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 import HeaderTabs from '../components/HeaderTabs';
 import SearchBar from '../components/SearchBar';
 import Categories from '../components/Categories';
-import RestaurantItem from '../components/RestaurantItem';
+import RestaurantItems from '../components/RestaurantItems';
 
 export default function Home() {
 	return (
@@ -14,7 +14,7 @@ export default function Home() {
 			</View>
 			<ScrollView showsVerticalScrollIndicator={false}>
 				<Categories />
-				<RestaurantItem />
+				<RestaurantItems />
 			</ScrollView>
 		</View>
 	);
